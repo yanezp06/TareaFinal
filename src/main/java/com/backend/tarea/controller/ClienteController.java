1c99f8e1650c80343dcab72f7ad7a07ba342317a
Andres <yanezp006@gmail.com>
2025-02-16 16:53:31 -0500

    Segunda modificacion

--- src/main/java/com/backend/tarea/controller/ClienteController.java
+++ src/main/java/com/backend/tarea/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/api/clientes") // Ruta base para Cliente
 @CrossOrigin(origins = "*") 
