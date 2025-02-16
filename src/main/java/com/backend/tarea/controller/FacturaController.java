833e985f85792c8315d9aa56491f0023cc3691b1
Andres <yanezp006@gmail.com>
2025-02-16 16:55:08 -0500

    Tercera modificacion

--- src/main/java/com/backend/tarea/controller/FacturaController.java
+++ src/main/java/com/backend/tarea/controller/FacturaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/api/facturas") // Ruta base para Factura
 @CrossOrigin(origins = "*") 
